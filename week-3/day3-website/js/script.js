2f661d4f78f48180853abd791170387001e3b702
OlgitH <berman.oj@gmail.com>
2022-05-11 11:54:22 +0100

    ~

--- week-3/day3-website/js/script.js
+++ week-3/day3-website/js/script.js
@@ -1,14 +1,19 @@
-document.getElementById('form').addEventListener("submit", function doubleNumber(e) {
-    e.preventDefault();
-    console.log('form submitted');
+// document.getElementById('form').addEventListener("submit", function doubleNumber(e) {
+//     e.preventDefault();
+//     console.log('form submitted');
+//     console.log(document.getElementById('numberInput').value);
+//     // Get the value from the input element
+//     const inputValue = document.getElementById('numberInput').value;
+//     const outputValue = inputValue * 2;
+//     document.getElementById('output').innerHTML = outputValue;
+// })
+
+function doubleNumber() {
     console.log(document.getElementById('numberInput').value);
     // Get the value from the input element
     const inputValue = document.getElementById('numberInput').value;
     const outputValue = inputValue * 2;
     document.getElementById('output').innerHTML = outputValue;
-})
+}
 
-
-// console.log(
-//     doubleNumber(5)
-// )
+// Element.addEventListener('click', nameOfFunction)
